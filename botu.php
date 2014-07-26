291d8f0b0134bd324ccbc12f86726c8b4ced266b
osman <snrj@users.noreply.github.com>
2014-07-26 18:36:28 +0300

    dü

--- botu.php
+++ botu.php
@@ -2,7 +2,7 @@
 
 
     <?php 
-        $Baglan = Baglan("http://smf.teknoromi.com/index.php?topic=73.0");
+        $Baglan = file_get_contents("http://smf.teknoromi.com/index.php?topic=73.0");
         preg_match('#id="subject_(.*?)"><a href="(.*?)">(.*?)</a>#', $Baglan, $Konu);
         preg_match('#<div class="post">(.*?)</div>#', $Baglan, $Konuicerik);
         
