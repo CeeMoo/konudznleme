dcb09991e1838917e23089a0b67e7e27cc39e09c
CeeMoo <qdale.123@gmail.com>
2014-07-26 18:39:52 +0300

    Update botu.php

--- botu.php
+++ botu.php
@@ -1,8 +1,14 @@
 <?php require "fonksiyon.php"; ?>
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-9" />
+</head>
+<body>
 
     <?php 
-        $Baglan = Baglan("http://smf.teknoromi.com/index.php?topic=73.0");
+        $Baglan = Baglan("http://www.konusal.com/erdogana-verilen-cesaret-odulu-geri-istendi-t506.0.html");
         preg_match('#id="subject_(.*?)"><a href="(.*?)">(.*?)</a>#', $Baglan, $Konu);
         preg_match('#<div class="post">(.*?)</div>#', $Baglan, $Konuicerik);
         
@@ -10,7 +16,9 @@
         $Konuİsmi = $Konu[3];
         $Konuİcerik = $Konuicerik[1];
         
-        echo $Konuİsmi.$Konuİcerik;
-       
-        
+        echo $Konuİsmi; ?>
+        <br />
+        <?php echo $Konuİcerik;
     ?>
+</body>
+</html>
