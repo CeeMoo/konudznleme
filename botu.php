5846f5f5e22a6615eedabe55a9d881819b7c039e
CeeMoo <qdale.123@gmail.com>
2014-07-26 18:34:16 +0300

    Create botu.php

---
+++ botu.php
@@ -0,0 +1,16 @@
+<?php require "fonksiyon.php"; ?>
+
+
+    <?php 
+        $Baglan = Baglan("http://smf.teknoromi.com/index.php?topic=73.0");
+        preg_match('#id="subject_(.*?)"><a href="(.*?)">(.*?)</a>#', $Baglan, $Konu);
+        preg_match('#<div class="post">(.*?)</div>#', $Baglan, $Konuicerik);
+        
+        
+        $Konuİsmi = $Konu[3];
+        $Konuİcerik = $Konuicerik[1];
+        
+        echo $Konuİsmi.$Konuİcerik;
+       
+        
+    ?>
